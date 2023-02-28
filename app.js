7bcb3ec8a5b10d531e2b06dcbdae7ae59b085a55
Rane Gray <ranegray830@gmail.com>
2023-02-28 22:31:24 +0100

    Add guess again functionality

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-const guessBtn = document.querySelector('#send-guess');
-const guessForm = document.querySelector('#guess');
-const result = document.querySelector('#result');
-let numToGuess = Math.floor(Math.random() * 10)
-let guess = 0;
-
-guessBtn.addEventListener('click', e => {
-    e.preventDefault();
-    guess = guessForm.value;
-    checkGuess(guess);
-})
+let numToGuess = Math.floor(Math.random() * 10) + 1;
+let userGuess = prompt("Guess a number between 1 and 10");
 
 function checkGuess(num) {
-    if (num > numToGuess){
-        result.textContent = 'Lower'
-    } else if (num < numToGuess){
-        result.textContent = 'Higher'
-    } else {
-        result.textContent = 'Correct!'
-    }
+  if (num > numToGuess) {
+    alert("Lower");
+    handleGuess();
+  } else if (num < numToGuess) {
+    alert("Higher");
+    handleGuess();
+  } else if (num == numToGuess) {
+    alert("Correct!");
+  } else {
+    alert("Not even close!");
+    handleGuess();
+  }
 }
 
-// prompt('Guess a number between 1 and 10')
+function handleGuess() {
+    userGuess = prompt('Guess again.')
+    checkGuess(userGuess);
+}
+
+console.log(numToGuess);
+console.log( typeof userGuess );
+checkGuess(userGuess);
+
+// const guessBtn = document.querySelector('#send-guess');
+// const guessForm = document.querySelector('#guess');
+// const result = document.querySelector('#result');
+// let guess = 0;
+
+// guessBtn.addEventListener('click', e => {
+//     e.preventDefault();
+//     guess = guessForm.value;
+//     checkGuess(guess);
+// })
